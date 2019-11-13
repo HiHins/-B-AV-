0745c8eea7da12ab3119365264a32fb1717d87c1
HiHins <hihins@sina.cn>
2019-11-13 10:30:11 +0800

    添加项目文件。

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 9F);
+            this.richTextBox2.Location = new System.Drawing.Point(13, 68);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(870, 354);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("华文细黑", 9F);
+            this.button2.Location = new System.Drawing.Point(678, 428);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "复制到剪切板";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(715, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("华文细黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(734, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 52);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "解析网址";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 475);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "提取B站AV号";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    String input = richTextBox1.Text;
+        // //   Regex regex = new Regex(@"av+\d{8}");
+        //    String regex=@"av+\d{8}";
+        //    String str = "";
+        //    foreach (Match match in Regex.Matches(input, regex))
+        //       // Console.WriteLine("https://www.bilibili.com/video/av" + match.Value);
+        //        str += "https://www.bilibili.com/video/av" + match.Value + "\n";
+        //    richTextBox2.Text = str;
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox2.Text);
+        }
+        private string GetWebClient(string url)
+        {
+            string strHTML = "";
+            try
+            {
+                WebClient myWebClient = new WebClient();
+                Stream myStream = myWebClient.OpenRead(url);
+                StreamReader sr = new StreamReader(myStream, System.Text.Encoding.GetEncoding("utf-8"));
+                strHTML = sr.ReadToEnd();
+                myStream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            return strHTML;
+        }
+        private string GetWebRequest(string url)
+        {
+            String strHTML = "";
+            try
+            {
+                Uri uri = new Uri(url);
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(url);
+                // WebRequest myReq = WebRequest.Create(uri);\
+                myReq.UserAgent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; QQWubi 133; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; CIBA; InfoPath.2)";
+                myReq.Method = "GET";
+                WebResponse result = myReq.GetResponse();
+                Stream receviceStream = result.GetResponseStream();
+                StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.GetEncoding("utf-8"));
+                strHTML = readerOfStream.ReadToEnd();
+                readerOfStream.Close();
+                receviceStream.Close();
+                result.Close();
+            }
+            catch (Exception e)
+            { 
+                MessageBox.Show("输入错误");
+            }
+            
+            return strHTML;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String str = GetWebRequest(textBox1.Text);
+           // Console.WriteLine(str);
+            String regex = @"av+\d{8}";
+            String str2 = "";
+            foreach (Match match in Regex.Matches(str, regex))
+                // Console.WriteLine("https://www.bilibili.com/video/av" + match.Value);
+                str2 += "https://www.bilibili.com/video/" + match.Value + "\n";
+            richTextBox2.Text = str2;
+            //richTextBox1.Text = str;
+            //button1_Click(null,null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.TabIndex = 0;
+            button3.TabIndex = 1;
+            richTextBox2.TabIndex = 2;
+            button2.TabIndex = 3;
+        }
+    }
+}
